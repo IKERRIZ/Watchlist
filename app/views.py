bbcc8d82c458fbacc28f1438b448ae158cbd00ef
IKERRIZ <okothfaith94@gmail.com>
2019-10-09 11:53:23 +0300

    Created a title variable

--- app/views.py
+++ app/views.py
@@ -3,14 +3,14 @@ from app import app
 
 #Views
 @app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+def index():
     
     '''
     View root page function that returns the index page and  its data
     '''
     
-    message = 'Hello World'
-    return render_template('movie.html', id =movie_id)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
 
 
 
